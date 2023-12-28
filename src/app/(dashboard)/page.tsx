073b78ebc82fb47f8224ed628e87a50f5fd99f2b
Matheus Palmieri <matheuspalmieridev@gmail.com>
2023-12-28 17:10:55 -0300

    feat: stats cards and prisma client

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,114 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getFormStats } from "../../../actions/form";
+import { LuView } from "react-icons/lu";
+import { FaWpforms } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+import { TbArrowBounce } from "react-icons/tb";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
+
 export default function Page() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="container pt-4">
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <CardStatsWrapper />
+      </Suspense>
+    </div>
+  );
 }
+
+const CardStatsWrapper = async () => {
+  try {
+    const stats = await getFormStats();
+    return <StatsCards loading={false} data={stats} />;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+interface StatsCardsProps {
+  data?: Awaited<ReturnType<typeof getFormStats>>;
+  loading: boolean;
+}
+
+const StatsCards = (props: StatsCardsProps) => {
+  const { data, loading } = props;
+
+  return (
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <StatsCard
+        title="Total visits"
+        value={data?.visits.toLocaleString() || ""}
+        icon={<LuView className="text-blue-600" />}
+        helperText="All time form visits"
+        loading={loading}
+        className="shadow-md shadow-blue-600"
+      />
+
+      <StatsCard
+        title="Total submissions"
+        value={data?.submissions.toLocaleString() || ""}
+        icon={<FaWpforms className="text-yellow-600" />}
+        helperText="All time form submissions"
+        loading={loading}
+        className="shadow-md shadow-yellow-600"
+      />
+
+      <StatsCard
+        title="Submission rate"
+        value={`${data?.submissionRate.toLocaleString()}%` || ""}
+        icon={<HiCursorClick className="text-green-600" />}
+        helperText="Visits that result in form submission"
+        loading={loading}
+        className="shadow-md shadow-green-600"
+      />
+
+      <StatsCard
+        title="Bounce rate"
+        value={`${data?.submissions.toLocaleString()}%` || ""}
+        icon={<TbArrowBounce className="text-red-600" />}
+        helperText="Visits that leaves without interacting"
+        loading={loading}
+        className="shadow-md shadow-red-600"
+      />
+    </div>
+  );
+};
+
+interface StatsCardProps {
+  title: string;
+  value: string;
+  icon: React.ReactNode;
+  helperText: string;
+  loading: boolean;
+  className: string;
+}
+
+const StatsCard = ({
+  title,
+  value,
+  icon,
+  helperText,
+  loading,
+  className,
+}: StatsCardProps) => (
+  <Card className={className}>
+    <CardHeader className="flex flex-row items-center justify-between pb-2">
+      <CardTitle className="text-sm font-medium text-muted-foreground">
+        {title}
+      </CardTitle>
+      {icon}
+    </CardHeader>
+    <CardContent>
+      <div className="text-2xl font-bold">
+        {loading && (
+          <Skeleton>
+            <span className="opacity-0">0</span>
+          </Skeleton>
+        )}
+        {!loading && value}
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </div>
+    </CardContent>
+  </Card>
+);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="pt-BR">
+      <html lang="en">
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
